892a895b57fe3eeca0ca0e9ac97eacc77dabdd0b
Cappy <42177607+Capp3@users.noreply.github.com>
2021-02-23 14:49:11 +0000

    Update create_net.sh

--- homeland/create_net.sh
+++ homeland/create_net.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Setup Marketplace Overlay Network"
 docker network create --driver=overlay --attachable marketplace
+echo "Setup Warehouse Overlay Network"
+docker network create --driver=overlay --attachable warehouse
 echo "The Docker Networks On This Machine Are:"
 docker network ls
