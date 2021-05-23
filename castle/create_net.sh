3ed790adf086893d7b91ef6d94ded3d67d8b3597
Cappy <42177607+Capp3@users.noreply.github.com>
2021-05-23 22:33:10 +0100

    Update create_net.sh

--- castle/create_net.sh
+++ castle/create_net.sh
@@ -3,5 +3,6 @@ echo "Setup Marketplace Overlay Network"
 docker network create --driver=overlay --attachable marketplace
 echo "Setup Warehouse Overlay Network"
 docker network create --driver=overlay --attachable warehouse
+docker network create --driver=overlay --attachable socket_proxy
 echo "The Docker Networks On This Machine Are:"
 docker network ls
