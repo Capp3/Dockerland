e15ceb2549402aa8130ff47f73d09f44660b3199
Cappy <42177607+Capp3@users.noreply.github.com>
2021-08-20 22:06:18 +0100

    Delete create_net.sh

--- castle/create_net.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Setup Marketplace Overlay Network"
-docker network create --driver=overlay --attachable --gateway=192.168.90.1 --subnet=192.168.90.0/24 marketplace
-echo "Setup Warehouse Overlay Network"
-docker network create --driver=overlay --attachable --gateway=192.168.91.1 --subnet=192.168.91.0/24 warehouse
-docker network create --driver=overlay --attachable --gateway=192.168.92.1 --subnet=192.168.92.0/24 socket_proxy
-echo "The Docker Networks On This Machine Are:"
-docker network ls
