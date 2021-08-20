6ce5a347fb915df687cf5ce5210c7b6ce8b51d13
Cappy <42177607+Capp3@users.noreply.github.com>
2021-08-20 22:55:36 +0100

    Update create_net.sh

--- castle/create_net.sh
+++ castle/create_net.sh
@@ -2,7 +2,7 @@
 echo "Setup Rollcall"
 docker network create -d macvlan \
   --subnet=192.168.1.0/24 \
-  --ip-range=192.168.1.100/30 \
+  --ip-range=192.168.1.111/28 \
   --gateway=192.168.1.1 \
   -o parent=enp1s0 rollcall
 echo "List of Docker Networks"
