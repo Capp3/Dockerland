c5024b05e5f6317c57d9c1feb9ab0d2f06e06e28
Cappy <42177607+Capp3@users.noreply.github.com>
2023-05-28 22:39:44 +0100

    Create swag.sh

---
+++ knight0/swag/swag.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+sudo rm /home/server/data/swag/nginx/proxy-confs/assets.subdomain.conf
+sudo rm /home/server/data/swag/nginx/proxy-confs/projects.subdomain.conf
+sudo rm /home/server/data/swag/nginx/proxy-confs/requests.subdomain.conf
+sudo ln-s ./assets.conf /home/server/data/swag/nginx/proxy-confs/assets.subdomain.conf
+sudo ln-s ./projects.conf /home/server/data/swag/nginx/proxy-confs/projects.subdomain.conf
+sudo ln-s ./requests.conf /home/server/data/swag/nginx/proxy-confs/requests.subdomain.conf
