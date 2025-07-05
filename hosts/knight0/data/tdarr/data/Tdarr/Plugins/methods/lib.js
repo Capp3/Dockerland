8bfec19e6dc9729819be4e6a3225a402d7c4acc1
capp3 <dcapp3@gmail.com>
2025-07-05 14:48:18 +0100

    ups

--- hosts/knight0/data/tdarr/data/Tdarr/Plugins/methods/lib.js
+++
@@ -1,3 +0,0 @@
-const importFresh = require('./node_modules/import-fresh');
-// load library modules fresh so no Tdarr Server restart required between plugin updates
-module.exports = () => importFresh('./library.js');
--- hosts/knight0/data/tdarr/data/Tdarr/Plugins/methods/library.js
+++
@@ -1,5 +0,0 @@
-const importFresh = require('./node_modules/import-fresh');
-
-module.exports.filters = importFresh('./filters.js');
-module.exports.actions = importFresh('./actions.js');
-module.exports.loadDefaultValues = importFresh('./loadDefaultValues.js');
